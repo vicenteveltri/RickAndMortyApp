87444bcbe44c745c2b17f487e5c855b0542b6ec3
Vicente Veltri <vicente.veltri.parser@ft.com>
2024-10-31 13:54:32 -0300

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Vicente Veltri on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
